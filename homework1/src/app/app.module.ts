0864c298e55ff32f39ba4c0ccc2f19fb28d58b0c
Kiore <ricardochicas95@gmail.com>
2019-12-29 19:23:09 -0600

    configure modules

--- homework1/src/app/app.module.ts
+++ homework1/src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LayoutModule } from "./module/layout/layout.module";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- homework1/src/app/module/layout/layout.module.ts
+++ homework1/src/app/module/layout/layout.module.ts
@@ -1,14 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LayoutComponent } from './layout/layout.component';
-import { FormComponent } from './form/form.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { LayoutComponent } from "./layout/layout.component";
+import { FormComponent } from "./form/form.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [LayoutComponent, FormComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, FormsModule],
+  exports: [LayoutComponent, FormComponent]
 })
-export class LayoutModule { }
+export class LayoutModule {}
