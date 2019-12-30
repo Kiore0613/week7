159173b9b5e5ac5659396b8cc1968daf99d8a278
Kiore <ricardochicas95@gmail.com>
2019-12-29 19:16:54 -0600

    Add global  Sass files

--- homework1/src/app/module/layout/layout.module.ts
+++ homework1/src/app/module/layout/layout.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout/layout.component';
+import { FormComponent } from './form/form.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LayoutComponent, FormComponent],
   imports: [
     CommonModule
   ]
