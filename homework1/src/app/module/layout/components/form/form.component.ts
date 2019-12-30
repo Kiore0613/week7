252fe30d9231f7f7df2781dd1126d6ddc7b95b23
Kiore <ricardochicas95@gmail.com>
2019-12-30 15:06:53 -0600

    add validators

--- homework1/src/app/module/layout/components/form/form.component.ts
+++ homework1/src/app/module/layout/components/form/form.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  selector: "app-form",
+  templateUrl: "./form.component.html",
+  styleUrls: ["./form.component.scss"]
 })
 export class FormComponent implements OnInit {
+  form = new FormGroup({
+    name: new FormControl("", [
+      Validators.maxLength(4),
+      Validators.minLength(15),
+      Validators.required
+    ])
+  });
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- homework1/src/app/module/layout/layout.module.ts
+++ homework1/src/app/module/layout/layout.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { LayoutComponent } from "./components/layout/layout.component";
 import { FormComponent } from "./components/form/form.component";
 
 @NgModule({
   declarations: [LayoutComponent, FormComponent],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   exports: [LayoutComponent, FormComponent]
 })
 export class LayoutModule {}
