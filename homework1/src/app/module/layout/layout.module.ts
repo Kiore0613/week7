496299b6cbd5e74bc80b8eaf07142e81be83b90e
Kiore <ricardochicas95@gmail.com>
2019-12-30 13:33:52 -0600

    fix scss routes and components

--- homework1/src/app/module/layout/layout.module.ts
+++ homework1/src/app/module/layout/layout.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { LayoutComponent } from "./layout/layout.component";
-import { FormComponent } from "./form/form.component";
 import { FormsModule } from "@angular/forms";
+import { LayoutComponent } from "./components/layout/layout.component";
+import { FormComponent } from "./components/form/form.component";
 
 @NgModule({
   declarations: [LayoutComponent, FormComponent],
